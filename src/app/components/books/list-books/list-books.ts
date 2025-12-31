30bb532857164237f4df4608d6db542a0a467d00
Deividas Martinkus <147333334+mdeividas19@users.noreply.github.com>
2025-12-31 04:21:26 +0200

    Filtering by title and isbn

--- src/app/components/books/list-books/list-books.ts
+++ src/app/components/books/list-books/list-books.ts
@@ -3,25 +3,31 @@ import { BooksService } from '../../../services/books-service';
 import { Book } from '../../../models/book';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-list-books',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './list-books.html',
   styleUrl: './list-books.css',
 })
 export class ListBooks {
   public books:Book[] = [];
+  public booksList:Book[] = [];
   public isLoading = false;
   public isError = false;
   public errorMessage = '';
+
+  public filterTitle = '';
+  public filterISBN = '';
 
   constructor (private booksService: BooksService, private router: Router) {
     this.isLoading = true;
     this.booksService.getBooks().subscribe({
       next:(data)=> {      
-        this.books = data;
+        this.booksList = data;
         this.isLoading = false;
+        this.filterBooks();
       },
       error:(data)=> {
         this.isLoading = false;
@@ -46,6 +52,18 @@ export class ListBooks {
           this.errorMessage = "Failed to delete book";
         }
       });
+    }
+  }
+
+  public filterBooks() {
+    if (this.filterTitle !== '') {
+      this.books = this.booksList.filter((book)=>(book.title?.toLowerCase().includes(this.filterTitle.toLowerCase())));
+    } else {
+      this.books = this.booksList;
+    }
+
+    if (this.filterISBN !== '') {
+      this.books = this.books.filter((book)=>(book.isbn?.toLowerCase().includes(this.filterISBN.toLowerCase())));
     }
   }
 }
